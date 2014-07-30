06095896d79d5fb050bab4c4120ddc8eecd0b814
raymoo <notanemail@fakeaddress.net>
2014-07-29 19:13:27 -0700

    changed temp default room from yfc to techcode

--- Pins/Handle.hs
+++ Pins/Handle.hs
@@ -11,5 +11,5 @@ handle :: String -> [Action]
 handle = makeAction . parseMessage
 
 makeAction :: Message -> [Action]
-makeAction (ChallStr ckey chall) = [Print "Received Challenge", Login ckey chall, Print "Sending response", Send "|/join yuyukofanclub"]
+makeAction (ChallStr ckey chall) = [Print "Received Challenge", Login ckey chall, Print "Sending response", Send "|/join techcode"]
 makeAction m = [Print "Unhandled Message"]
