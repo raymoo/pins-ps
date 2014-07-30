cfb79d35dbb4013b65010dff357e7fd2a1d4f69e
raymoo <notanemail@fakeaddress.net>
2014-07-29 18:04:03 -0700

    Main module - loads from basic config

---
+++ Pins.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Pins.Bot
+
+generateConfig :: [String] -> Config
+generateConfig ss = Config (arg 0) (arg 1) (arg 2) (read . arg $ 3) (arg 4) --VERY UGLY AND BAD FUNCTION, CHANGE TO USE YAML OR SOMETHING LATER
+    where arg = (ss!!)
+
+runWhat :: [String] -> IO ()
+runWhat ss
+    | length ss < 5 = putStrLn "Invalid Config File"
+    | otherwise = runBot . generateConfig $ ss
+
+main :: IO ()
+main = readFile "config" >>= runWhat . lines
