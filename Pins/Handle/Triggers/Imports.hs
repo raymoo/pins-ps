1a86b51eff92043aca6f2a5918aced2e86f980fe
raymoo <uguu@installgentoo.com>
2014-07-30 22:59:54 -0700

    File for trigger imports

---
+++ Pins/Handle/Triggers/Imports.hs
@@ -0,0 +1,6 @@
+module Pins.Handle.Triggers.Imports (module All) where
+
+import Pins.Bot.Inputs as All hiding (ConstantString)
+
+import Pins.Handle.Actions as All
+import Pins.Handle.Actions.Base (Action)
