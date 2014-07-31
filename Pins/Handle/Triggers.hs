9d6385826ec68b8c7ec13c11a4b2ce90f174d1f2
raymoo <uguu@installgentoo.com>
2014-07-30 23:15:57 -0700

    Basic datatypes for triggers

---
+++ Pins/Handle/Triggers.hs
@@ -0,0 +1,17 @@
+module Pins.Handle.Triggers where
+
+import Pins.Handle.Triggers.Imports
+
+data MessageInfo = MessageInfo { mType  :: String        -- What was it - chat, pm, join message, etc?
+                               , what   :: String        -- The content
+                               , who    :: String        -- The user
+                               , rank   :: Char          -- The user's rank
+                               , room   :: String        -- The room the message was in
+                               , inputs :: [InputResult] --Any inputs
+                               }
+
+data Trigger = Trigger [Input]                   -- Any inputs the trigger needs
+                       (MessageInfo -> Bool)     -- The checking function
+                       (MessageInfo -> [Action]) -- The acting function
+
+
