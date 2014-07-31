2b50fe9cfde5883bddf8253bfa7edc867243d427
raymoo <uguu@installgentoo.com>
2014-07-30 23:15:00 -0700

    fix import

--- Pins/Handle/Triggers/Imports.hs
+++ Pins/Handle/Triggers/Imports.hs
@@ -1,6 +1,6 @@
 module Pins.Handle.Triggers.Imports (module All) where
 
-import Pins.Bot.Inputs as All hiding (ConstantString)
+import Pins.Bot.Inputs as All hiding (ConstantString, Constantable)
 
 import Pins.Handle.Actions as All
-import Pins.Handle.Actions.Base (Action)
+import Pins.Handle.Actions.Base as All (Action)
