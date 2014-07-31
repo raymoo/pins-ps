1f38c3d78ca798dd1cbc1161c5d1a757207fe850
raymoo <uguu@installgentoo.com>
2014-07-31 00:00:23 -0700

    Fleshed out more trigger utilities, fixed type of inputs to be alists

--- Pins/Handle/Triggers.hs
+++ Pins/Handle/Triggers.hs
@@ -2,16 +2,41 @@ module Pins.Handle.Triggers where
 
 import Pins.Handle.Triggers.Imports
 
-data MessageInfo = MessageInfo { mType  :: String        -- What was it - chat, pm, join message, etc?
-                               , what   :: String        -- The content
-                               , who    :: String        -- The user
-                               , rank   :: Char          -- The user's rank
-                               , room   :: String        -- The room the message was in
-                               , inputs :: [InputResult] -- Result of any inputs
+data MessageInfo = MessageInfo { mType  :: String                  -- What was it - chat, pm, join message, etc?
+                               , what   :: String                  -- The content
+                               , who    :: String                  -- The user
+                               , rank   :: Char                    -- The user's rank
+                               , room   :: String                  -- The room the message was in
+                               , inputs :: [(String, InputResult)] -- Result of any inputs
                                }
 
-data Trigger = Trigger [Input]                   -- Any inputs the trigger needs
-                       (MessageInfo -> Bool)     -- The checking function
-                       (MessageInfo -> [Action]) -- The acting function
+type Test = (MessageInfo -> Bool)
+type Act = (MessageInfo -> [Action])
+type Key = String
 
+data Trigger = Trigger [(String, Input)] -- Any inputs the trigger needs, with names
+                       Test              -- The checking function
+                       Act               -- The acting function
 
+-- Utility Functions: Common Tests
+contentIs :: String -> Test
+contentIs s = (s==) . what
+
+startsWith :: String -> Test
+startsWith s = and . zipWith (==) s . what
+
+-- Utility Functions: Etc
+maybeString :: Key -> MessageInfo -> Maybe String
+maybeString k mi = case couldBeString of
+                     Just (InputString s) -> Just s
+                     _                    -> Nothing
+    where couldBeString = lookup k (inputs mi)
+
+single :: a -> [a]
+single x = [x]
+
+-- Test trigger: Tests current basic functionality
+testCheck :: Trigger
+testCheck = Trigger [("testString", constant "basic input system works")]
+                    (contentIs "!test")
+                    (single . put . maybe "not this" ("The test string is: "++) . maybeString "testString")
