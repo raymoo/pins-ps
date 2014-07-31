33df00bae5400217ffb6e4b9569ae9e72ff0104a
raymoo <uguu@installgentoo.com>
2014-07-31 01:23:39 -0700

    Try to accomodate triggers

--- Pins/Handle.hs
+++ Pins/Handle.hs
@@ -2,7 +2,35 @@ module Pins.Handle ( Action(..), handle ) where
 
 import Pins.Handle.Parse
 import Pins.Handle.Actions.Base
+import Pins.Handle.Actions
 import Pins.Handle.Triggers
+import Pins.Bot.Inputs
+
+import Data.Maybe
+
+triggerList :: [Trigger]
+triggerList = [testCheck]
+
+passTriggers :: MessageInfo -> [Trigger] -> [Action]
+passTriggers mi = concatMap (doTrigger mi) . filter (checkTrigger mi)
+
+checkTrigger :: MessageInfo -> Trigger -> Bool
+checkTrigger mi t = (test t) mi
+
+doTrigger :: MessageInfo -> Trigger -> [Action]
+doTrigger mi t = act t (mi { inputs = getInputResults (tInputs t) })
+
+defaultMInfo :: MessageInfo
+defaultMInfo = MessageInfo "idleThoughts"
+                           ""
+                           ""
+                           ' '
+                           ""
+                           Print
+
+
+
+                           []
 
 handle :: String -> [Action]
 handle = makeAction . parseMessage
@@ -13,4 +41,24 @@ makeAction (ChallStr ckey chall) = [ Print "Received Challenge"
                                    , Print "Sending response"
                                    , Send "|/join techcode"
                                    ]
+makeAction c@(Chat _ _ _) = passTriggers (makeMInfo c) triggerList
 makeAction m = [Print "Unhandled Message"]
+
+safeHead :: [a] -> Maybe a
+safeHead (x:xs) = Just x
+safeHead []     = Nothing
+
+makeMInfo :: Message -> MessageInfo
+makeMInfo (Chat r u w) = defaultMInfo { mType = "c"
+                                 , what = w
+                                 , who  = drop 1 u
+                                 , rank = fromMaybe ' ' (safeHead u)
+                                 , room = r
+                                 , respond = sendChat r
+                                 }
+
+getInputResults :: [(String, Input)] -> [(String, InputResult)]
+getInputResults xs = zip (map fst xs) (map (getInputResult . snd) xs)
+
+getInputResult :: Input -> InputResult --Placeholder Function
+getInputResult (ConstantString s) = InputString s
