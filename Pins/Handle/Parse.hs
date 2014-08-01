2ef1669c8c9b44cf4e04ae4eac342385dba12bbe
raymoo <uguu@installgentoo.com>
2014-07-31 17:35:08 -0700

    Added a parser that can handle messages with pipes, and also message parser now handles PMs

--- Pins/Handle/Parse.hs
+++ Pins/Handle/Parse.hs
@@ -22,12 +22,15 @@ maybeRead = fmap fst . listToMaybe . reads
 
 -- Basic Parsers
 mLex :: Parser String
-mLex = char '|' >> inMessage
+mLex = char '|' *> inMessage
 
 dLex :: Parser Int
 dLex = maybeRead <$> mLex >>= check
     where check Nothing = unexpected "non-number"
           check (Just a) = return a
+
+contLex :: Parser String
+contLex = char '|' *> many anyChar
 
 room :: Parser String
 room = char '>' *> many (noneOf "\n") <* char '\n'
@@ -39,32 +42,31 @@ getArgs :: Parser [String]
 getArgs = many mLex
 
 -- Main message parser
+parseMessage :: String -> Message
+parseMessage s = case parse message "message parsing" s of
+                 Right a -> a
+                 Left _ -> Unknown
+
 message :: Parser Message
 message = try (room >>= chooseParse) <|> try (chooseParse "") <|> baseStr
 
 -- Deciders
 chooseParse :: String -> Parser Message
 chooseParse r = mLex >>= chooseMore
-    where chooseMore "c"        = chat r
+    where chooseMore "c"        = try (chat r) <|> return Unknown
           chooseMore "challstr" = try challStr <|> return Unknown
+          chooseMore "pm"       = try pm <|> return Unknown
           chooseMore _          = return Unknown
 
 -- Message type specific parsers
-
+pm :: Parser Message
+pm = Pm <$> mLex <*> (mLex *> contLex)
 
 chat :: String -> Parser Message
-chat r = (Chat r) <$> mLex <*> mLex
-
-
-
+chat r = (Chat r) <$> mLex <*> contLex
 
 challStr :: Parser Message
 challStr = ChallStr <$>  dLex <*> mLex
 
 baseStr :: Parser Message
 baseStr = Base <$> (many anyChar)
-
-parseMessage :: String -> Message
-parseMessage s = case parse message "message parsing" s of
-                 Right a -> a
-                 Left _ -> Unknown
