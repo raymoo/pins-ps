4bb075366d24b2e672346cbd40d94938bba543c7
raymoo <uguu@installgentoo.com>
2014-08-02 17:53:43 -0700

    Var datatype, for storing values that might be stored or retrieved by triggers

--- Pins/Handle/MonadAction.hs
+++ Pins/Handle/MonadAction.hs
@@ -1,13 +1,36 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Pins.Handle.MonadAction where
+
+import Data.Maybe
 
 type Room = String
 type User = String
 type Pass = String
 
+data Var = VarString String
+         | VarArray [Var]
+
+class Variable a where
+    pack   :: a -> Var
+    unpack :: Var -> Maybe a
+
+instance Variable String where
+    pack = VarString
+    unpack (VarString s) = Just s
+    unpack _             = Nothing
+
+instance Variable a => Variable [a] where
+    pack = VarArray . map pack
+    unpack (VarArray xs) = Just . catMaybes $ map unpack xs
+    unpack _             = Nothing
+
 class Monad m => MonadAction m where
     send :: String -> m ()
     login :: Int -> String -> m ()
     printLn :: String -> m () 
+    putVar :: String -> Var -> m ()
+    getVar :: String -> m Var
 
 constant :: MonadAction m => a -> m a
 constant = return
@@ -22,5 +45,3 @@ sendPm u m = command ("/pm " ++ u ++ ',' : m)
 command :: MonadAction m => String -> m ()
 command = send . ('|' :)
 
-put :: MonadAction m => String -> m ()
-put = printLn
