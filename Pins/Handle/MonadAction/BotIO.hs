8708d071a5f1ce37d5c8579890f54e26df9bd1ae
raymoo <uguu@installgentoo.com>
2014-08-02 18:55:20 -0700

    forgot to add this file to git. It is an implementation of MonadAction in a StateT Bot IO

---
+++ Pins/Handle/MonadAction/BotIO.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Pins.Handle.MonadAction.BotIO where
+
+import           Pins.Bot.Login
+import           Pins.Handle.MonadAction
+import           Control.Monad.State.Lazy
+import           Control.Applicative
+import qualified Network.WebSockets       as WS
+import qualified Data.Text                as T
+import qualified Data.Map                 as M
+
+data Bot = Bot { bName :: String
+               , bPass :: String 
+               , bConn :: WS.Connection
+               , vars  :: M.Map String Var
+               }
+
+blankVar :: M.Map String Var
+blankVar = M.empty
+
+getName :: StateT Bot IO String
+getName = bName <$> get
+
+getPass :: StateT Bot IO String
+getPass = bPass <$> get
+
+getConn :: StateT Bot IO WS.Connection
+getConn = bConn <$> get
+
+getVars :: StateT Bot IO (M.Map String Var)
+getVars = vars <$> get
+
+instance MonadAction (StateT Bot IO) where
+    send s = getConn >>= (\b ->
+                          liftIO . WS.sendTextData b . T.pack $ s)
+    printLn = liftIO . putStrLn
+    login cKey chall = do
+      name <- getName
+      pass <- getPass
+      assertion <- lift $ getAssertion name pass cKey chall
+      command ("/trn " ++ name ++ ",0," ++ assertion)
+    putVar k x = modify (\s -> s {vars = M.insert k x $ vars s})
+    getVar k = M.lookup k <$> getVars
