34bf6b80afa3ceedfa3b08c9a64e1b3146077bd5
raymoo <uguu@installgentoo.com>
2014-08-03 17:45:12 -0700

    Default config

--- Pins.hs
+++ Pins.hs
@@ -1,17 +1,34 @@
 module Main where
 
 import Pins.Bot
+import Control.Monad
+import System.Directory
 
 generateConfig :: [String] -> Config
 generateConfig ss = Config (arg 0) (arg 1) (arg 2) (read . arg $ 3) (arg 4) --VERY UGLY AND BAD FUNCTION, CHANGE TO USE YAML OR SOMETHING LATER
     where arg = (ss!!)
 
-runWhat :: [String] -> IO ()
-runWhat ss
-    | length ss < 5 = putStrLn "Invalid Config File"
-    | otherwise = runBot . generateConfig $ ss
+testConfig :: [String] -> IO Config
+testConfig ss
+    | length ss < 5 = putStrLn "Config File too short." >>
+                      defaultConfig
+    | otherwise = return . generateConfig $ ss
+
+getConfig :: Bool -> IO Config
+getConfig True  = readFile "config" >>= testConfig . lines
+getConfig False = defaultConfig
+
+defaultConfig :: IO Config
+defaultConfig = putStrLn "No valid config file found, defaulting to Smogon server" >>
+                liftM2 (\x y -> Config x y "sim.smogon.com" 8000 "/showdown/websocket")
+                (prompt "Username?")
+                (prompt "Password?")
+
+prompt :: String -> IO String
+prompt s = putStrLn s >>
+           getLine
 
 main :: IO ()
 main = 
-    readFile "config" >>=
-    runWhat . lines
+    doesFileExist "config" >>=
+    getConfig >>= runBot
